a33284482a39a249e26907f4aac027582d68d8ba
ClementChaumel <clement.chaumel@gmail.com>
2016-04-15 14:48:55 +0200

    test LoginGenerator

--- src/test/java/dcll/LoginGeneratorTest.java
+++ src/test/java/dcll/LoginGeneratorTest.java
@@ -7,13 +7,27 @@ import static org.junit.Assert.*;
 
 public class LoginGeneratorTest {
 
+    LoginService loginService;
+    LoginGenerator generator;
+
     @Before
     public void setUp() throws Exception {
-
+        loginService = new LoginService(new String[] {"JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"});
+        generator = new LoginGenerator(loginService);
     }
 
     @Test
     public void testGenerateLoginForNomAndPrenom() throws Exception {
+        assertEquals("PDUR", generator.generateLoginForNomAndPrenom("Durand", "Paul"));
+    }
 
+    @Test
+    public void testGenerateLoginForNomAndPrenomExistant() throws Exception {
+        assertEquals("JROL1", generator.generateLoginForNomAndPrenom("Rolling", "Jean"));
+    }
+
+    @Test
+    public void testGenerateLoginForNomAndPrenomAccent() throws Exception {
+        assertEquals("PDUR", generator.generateLoginForNomAndPrenom("Dùrand", "Paul"));
     }
 }
