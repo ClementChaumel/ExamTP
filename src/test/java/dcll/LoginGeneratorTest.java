d8525af4f2a1c306dae27d4637cd16f01c09f20e
ClementChaumel <clement.chaumel@gmail.com>
2016-04-15 14:47:43 +0200

    creation de la classe de test pour LoginGenerator

---
+++ src/test/java/dcll/LoginGeneratorTest.java
@@ -0,0 +1,19 @@
+package dcll;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGenerateLoginForNomAndPrenom() throws Exception {
+
+    }
+}
